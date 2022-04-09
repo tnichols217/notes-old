32186f4b2b29880ce36ea36185bf233933111862
tnichols <tnichols@student.dalat.org>
2022-04-09 22:36:51 +0800

    vault backup: 2022-04-09 22:36:51

--- .obsidian/plugins/obsidian-git/main.js
+++ .obsidian/plugins/obsidian-git/main.js
@@ -10831,7 +10831,7 @@ var SimpleGit = class extends GitManager {
     return __async(this, null, function* () {
       if (this.plugin.settings.updateSubmodules) {
         this.plugin.setState(PluginState.commit);
-        yield this.git.outputHandler((x) => console.log(x)).subModule(["foreach", "--recursive", `git add -A && if [ ! -z "$(git status --porcelain)" ]; then git commit -m "${yield this.formatCommitMessage(message)}"; fi`], (err) => this.onError(err));
+        yield this.git.outputHandler((x, y, z) => console.log(x, y, z)).subModule(["foreach", "--recursive", `git add -A && if [ ! -z "$(git status --porcelain)" ]; then git commit -m "${yield this.formatCommitMessage(message)}"; fi`], (err) => this.onError(err));
       }
       this.plugin.setState(PluginState.add);
       yield this.git.add("-A", (err) => this.onError(err));
