493bb2e2b5f504e4113251c4f2544f0a596bb20a
tnichols <tnichols@student.dalat.org>
2022-04-10 03:07:18 +0800

    vault backup: 2022-04-10 03:07:18

--- .obsidian/plugins/obsidian-git/main.js
+++ .obsidian/plugins/obsidian-git/main.js
@@ -10833,7 +10833,7 @@ var SimpleGit = class extends GitManager {
         this.plugin.setState(PluginState.commit);
         this.git.outputHandler((x, y, z) => __async(this, null, function* () {
           let body = "";
-          let roo = process.cwd();
+          let roo = this.git.cwd;
           y.on("data", (chunk) => {
             body += chunk.toString("utf8");
           });
