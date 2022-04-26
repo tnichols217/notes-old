28d410641293994ef0a0e6a1722b4a868a779222
tnichols <tnichols@student.dalat.org>
2022-04-26 12:14:53 +0800

    vault backup: 2022-04-26 12:14:53

--- .obsidian/plugins/obsidian-columns/main.js
+++ .obsidian/plugins/obsidian-columns/main.js
@@ -87,7 +87,7 @@ var ObsidianColumns = class extends import_obsidian.Plugin {
               if (j.nodeName != "UL" && el.nodeName != "OL") {
                 return;
               }
-              let span = j.textContent.split("\n")[0];
+              let span = j.textContent.split("\n");
               console.log(span);
             });
             console.log(subel);
