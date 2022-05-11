646ddc64946ae894d261fcfa75bd441ddbab5edb
tnichols <tnichols@student.dalat.org>
2022-05-11 11:19:40 +0800

    vault backup: 2022-05-11 11:19:40

--- .obsidian/plugins/obsidian-dynamic-import/main.js
+++ .obsidian/plugins/obsidian-dynamic-import/main.js
@@ -3062,14 +3062,13 @@ var ObsidianExternalEmbed = class extends import_obsidian.Plugin {
     };
     this.processURI = (URI, source, root) => {
       URI = URI.split("<")[0];
-      console.log(URI, source.substring(0, source.lastIndexOf("/")), root);
       if (!URI.contains("://")) {
         if (URI.startsWith("/")) {
           return [URISCHEME, URI].join("");
         } else if (URI.startsWith("./")) {
-          return [URISCHEME, source.substring(0, source.lastIndexOf("/")), "/", URI.substring(2)].join("");
+          return [URISCHEME, "/", source.substring(0, source.lastIndexOf("/")), "/", URI.substring(2)].join("");
         } else {
-          return [URISCHEME, source.substring(0, source.lastIndexOf("/")), "/", URI].join("");
+          return [URISCHEME, "/", source.substring(0, source.lastIndexOf("/")), "/", URI].join("");
         }
       }
       return URI;
